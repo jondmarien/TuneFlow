bd65e5fac642a6b1935d314aa39fb78ff1948afe
Jon Marien <jondmarien@gmail.com>
2025-04-16 16:22:42 -0400

    Added CORS URL so I dont get any allowedDevOrigins issues.

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: ['tuneflow.chron0.tech'],
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
